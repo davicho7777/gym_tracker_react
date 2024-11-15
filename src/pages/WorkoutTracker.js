9911adf743c5fe29bf6acd2873ccb133b1ade0cc
davicho7777 <140481248+davicho7777@users.noreply.github.com>
2024-11-15 18:12:54 -0300

    Update WorkoutTracker.js

--- src/pages/WorkoutTracker.js
+++ src/pages/WorkoutTracker.js
@@ -2,9 +2,9 @@
 
 import React, { useState, useEffect } from 'react'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
-import  Button  from '@components/ui/button'
-import  Input  from '@components/ui/input'
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@components/ui/table'
+import  Button  from '../components/ui/button'
+import  Input  from '../components/ui/input'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../components/ui/table'
 import { Edit2, Check, X, Plus, Minus } from 'lucide-react'
 
 
