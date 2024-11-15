a61945db763b51516e249eb288c353d2c6a81469
davicho7777 <140481248+davicho7777@users.noreply.github.com>
2024-11-15 17:35:14 -0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  webpack(config) {
-    config.resolve.alias['@components'] = path.join(__dirname, 'src/components');
-    return config;
-  },
-};
